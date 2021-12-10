95f92c10e7de9bb41d86e1887601ff4da1617f05
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-10 11:59:06 +0530

    PrimeNumbers Displaying between 0 to 1000 Done

---
+++ PrimeNum(0,1000)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrimeNum_0_1000_
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            bool isPrime = true;
+            Console.WriteLine("Prime Numbers : ");
+            for (int i = 2; i <= 1000; i++)
+            {
+                for (int j = 2; j <= 1000; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+        }
+    }
+}
